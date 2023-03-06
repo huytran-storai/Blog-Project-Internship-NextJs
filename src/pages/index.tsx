dc9388dfaa10a31e1a912645cc52e3b2a5408a01
huytran-storai <huy.t@stor.ai>
2023-03-06 16:56:28 +0700

    setup du an

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main className="w-screen h-screen">
+      <title>Home Page</title>
+      <section>
+        <div className="mt-3 text-center">
+          <h1 className="text-[3rem]">Welcome to DevBlog</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequatur, quam perspiciatis! Dolor nostrum reprehenderit nulla
+            nesciunt facere quidem rem. Quidem quia obcaecati sit tenetur
+            laudantium nesciunt voluptate, inventore praesentium quas!
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
